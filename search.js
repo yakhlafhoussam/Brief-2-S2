550d16c609c9e9dd2b48c67c4719c4ec57a6f919
yakhlafhoussam <yakhlafhoussam@gmail.com>
2025-11-10 12:57:37 +0100

    Prepare the onclick logic of the card and same menu page

--- search.js
+++ search.js
@@ -1,6 +1,13 @@
+let cards = document.querySelectorAll(".cards");
+
 document.getElementById("home").onclick = function () {
     window.location.href = "index.html";
 }
 document.getElementById("homeicon").onclick = function () {
     window.location.href = "index.html";
 }
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        window.location.href = "detail.html";
+    });
+});

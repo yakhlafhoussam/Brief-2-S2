2cf1215b716fa0e69c88002d41b95ed9dc7525a8
yakhlafhoussam <yakhlafhoussam@gmail.com>
2025-11-10 09:24:45 +0100

    Try to Fix switch problem

---
+++ detail.js
@@ -0,0 +1,6 @@
+document.getElementById("search").onclick = function () {
+    window.location.href = "search.html";
+}
+document.getElementById("home").onclick = function () {
+    window.location.href = "index.html";
+}
